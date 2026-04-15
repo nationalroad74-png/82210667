eb2f8300591d5c40746058163bd310410bb3de09
정국도 <nationalroad74@gmail.com>
2026-04-15 11:10:07 +0900

    Azure step

---
+++ src/main/java/com/example/sampleapp1/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.sampleapp1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    private static final String EMPLOYEE_ID = "[사번]";
+
+    @GetMapping("/user")
+    public Map<String, String> getUser() {
+        return Map.of(
+                "employeeId", EMPLOYEE_ID
+        );
+    }
+}
